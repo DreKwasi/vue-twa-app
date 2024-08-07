7553e75227719915e6dc9956f35d67010358135c
DreKwasi <andrewsboateng137@gmail.com>
2024-08-07 02:53:49 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import "./assets/main.css";
+import { VueTelegramPlugin } from "vue-tg";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(VueTelegramPlugin);
+app.mount("#app");
+
